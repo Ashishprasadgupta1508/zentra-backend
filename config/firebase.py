7c4998b114c9e7ad070bf0ca62460b51db26b2fa
Ashish <ashishprasadgupta38@gmail.com>
2026-06-25 19:32:24 +0530

    Update firebase.py

--- config/firebase.py
+++ config/firebase.py
@@ -1,14 +1,13 @@
+import os
 import firebase_admin
 from firebase_admin import credentials
-import os
 
 if not firebase_admin._apps:
 
+    BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
     cred = credentials.Certificate(
-        os.path.join(
-            os.path.dirname(__file__),
-            "firebase-service-account.json"
-        )
+        os.path.join(BASE_DIR, "firebase-service-account.json")
     )
 
     firebase_admin.initialize_app(cred)
