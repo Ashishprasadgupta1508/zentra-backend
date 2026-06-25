239e5780f06fb2792d701b8ae2414c87d6f3326d
Ashish <ashishprasadgupta38@gmail.com>
2026-06-25 19:21:55 +0530

    Added SignupView

--- users/urls.py
+++ users/urls.py
@@ -1,27 +1,10 @@
 from django.urls import path
-
-from .views import (
-    VerifyUserView,
-    SignupView,
-    SendVerificationEmailView,
-)
+from .views import VerifyUserView
 
 urlpatterns = [
     path(
         "verify-user/",
         VerifyUserView.as_view(),
-        name="verify-user"
-    ),
-
-    path(
-        "signup/",
-        SignupView.as_view(),
-        name="signup"
-    ),
-
-    path(
-        "send-verification-email/",
-        SendVerificationEmailView.as_view(),
-        name="send-verification-email"
+        name="verify-user",
     ),
 ]
